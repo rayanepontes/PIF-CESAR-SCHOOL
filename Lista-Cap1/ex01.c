28847ca37d0e5da9a076b990f470e6f9a359e817
Rayane Pontes <rynemaria@gmail.com>
2026-08-12 21:38:34 -0300

    questões 1 e 2 concluidas - atividade 1

---
+++ Lista-Cap1/ex01.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int ano_nascimento = 2008;
+    printf("meu ano de nacimento é: %d\n", ano_nascimento);
+    scanf("%d", &ano_nascimento);
+    return 0;
+}
---
+++ Lista-Cap1/ex02.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    float euler = 2.718281828459045;
+    printf("O numero de Euler equivale a: %.3f\n", euler);
+
+    return 0;
+}
